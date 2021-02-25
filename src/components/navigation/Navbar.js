01ece8f1251fbedc183b53e33c2323f83fd216d5
xenaletters <62646699+xenaletters@users.noreply.github.com>
2021-02-25 22:28:13 +0100

    Menu without store

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -5,7 +5,6 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 
@@ -22,38 +21,48 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function ButtonAppBar() {
+function openShop(){
+
+}
+function openProfs(){
+
+}
+export default function NavBar() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
-        <Toolbar>
+        <Toolbar >
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
+          <Paper className={classes.paper}>
+              <Button  onClick={openShop} > Shop </Button>
+              <Button onClick={openProfs}> Profs </Button>
+              </Paper>
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            News
+            Uni- Simulator 2021
           </Typography>
-          <Grid  direction="column">
-            <Grid item xs={12} spacing={3}>
+          <Grid  direction="column" style={{width:"100px"}}>
+            <Grid item xs={12} spacing={3} style={{paddingTop:"5px",paddingBottom:"5px"}}>
             <Paper className={classes.paper}>
               <MoneyDisplay currency={"students"}></MoneyDisplay>
             </Paper>
             </Grid>
-            <Grid item xs={12} spacing={3}>
-            <Paper className={classes.paper}>
+            <Grid item xs={12} spacing={3} style={{paddingBottom:"5px"}}>
+            <Paper className={classes.paper} >
               <MoneyDisplay currency={"exmat."}></MoneyDisplay>
             </Paper>
             </Grid>
             
-            <Grid item xs={12} spacing={3}>
+            <Grid item xs={12} spacing={3}style={{paddingBottom:"5px"}}>
             <Paper className={classes.paper}>
               <MoneyDisplay currency={"degrees"}></MoneyDisplay>
               </Paper>
             </Grid>
           </Grid>
         </Toolbar>
+        
       </AppBar>
     </div>
   );
