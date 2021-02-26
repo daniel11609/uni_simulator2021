5254be3ddefc29e1ec76c54b38549b4514c8e768
BaderTim <bader.tim_alex-it19@it.dhbw-ravensburg.de>
2021-02-26 10:05:59 +0100

    updated gitignore and added bootschdräbb

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from "./components/navigation/Navbar"
 function App() {
   return (
     <div className="App">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+              integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl"
+              crossOrigin="anonymous"/>
       <Navbar/>
       <h1>Hier könnte Ihre Werbung stehen!</h1>
     </div>
