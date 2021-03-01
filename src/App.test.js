133b5ee256a109ff6ae1a82a4b9bba11c95a3c21
PC <PC@DESKTOP-V8J7PKQ>
2021-03-01 22:14:20 +0100

    fixed first 2 tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { act, render, screen, wait, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Loading page', () => {
+
   render(<App />);
-  const linkElement = screen.getByText(/Uni-Simulator 2021/i);
+
+  const linkElement = screen.getByText(/Loading.../i);
   expect(linkElement).toBeInTheDocument();
+
 });
+
+test('renders login page', async () => {
+
+  render(<App />);
+
+  
+
+  const headingElement = await screen.findByText(/Uni-Simulator 2021/i);
+
+  expect(headingElement).toBeInTheDocument();
+
+});
+
