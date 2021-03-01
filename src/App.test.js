c2145372aaf5edc7e3851ac3f46796d38b46430c
PC <PC@DESKTOP-V8J7PKQ>
2021-03-01 23:01:57 +0100

    added comments in app js tests

--- src/App.test.js
+++ src/App.test.js
@@ -11,10 +11,14 @@ test('renders Loading page', () => {
 
 test('renders login page', async () => {
 
-  // render the App component
+  // arrange
   render(<App />);
+
+
   // get the headline elemnt
   const headingElement = await screen.findByText(/Uni-Simulator 2021/i);
+
+  // assert
   // check if it is in the DOM
   expect(headingElement).toBeInTheDocument();
 
@@ -22,19 +26,19 @@ test('renders login page', async () => {
 
 
 
-test('test username too short error', async () => {
-
-  // arrange: render App; get text input
+test('test username length limits (too short)', async () => {
+  // arrange
+  
   render(<App />);
-
+  // get text input; play button
   const userNameInput = await screen.findByPlaceholderText(/Username/i);
+  const playButton = await screen.findByText(/Play/i);
 
   // act: input a username which is too short
   fireEvent.change(userNameInput, { target: { value: '00' } })
 
-
   const errorMsg = await screen.findByText(/Your username has to be longer than 3 characters./i);
-
+ 
   // assert: check if error msg get shown; check for value of username
   expect(errorMsg).toBeInTheDocument();
   expect(userNameInput.value).toBe('00');
@@ -42,37 +46,51 @@ test('test username too short error', async () => {
 });
 
 
-test('test username length check', async () => {
+test('test username length check (no errors expected)', async () => {
 
-  // arrange: render App; get text input
+  // arrange
+  // render App; get text input
   render(<App />);
-
+  // get input field and play button
   const userNameInput = await screen.findByPlaceholderText(/Username/i);
+  const playButton = await screen.findByText(/Play/i);
 
-  // act: input a username which is too short
+  // act
+  //input a username which is too short
   fireEvent.change(userNameInput, { target: { value: 'MarcelDavis' } })
 
-
-  // assert: check if value of username is set correctly
+  // assert
+  // check if value of username is set correctly
   expect(userNameInput.value).toBe('MarcelDavis');
+  // check if the play button gets rendered
+  expect(playButton).toBeInTheDocument();
+  // make sure play button is active
+  expect(playButton).toBeEnabled();
 
 });
 
 test('test username error (too long)', async () => {
 
-  // arrange: render App; get text input
+  
+  // render App; get text input
   render(<App />);
 
   const userNameInput = await screen.findByPlaceholderText(/Username/i);
+  const playButton = await screen.findByText(/Play/i);
 
-  // act: input a username which is too long (>20 characters)
+  // act
+  // input a username which is too long (>20 characters)
   fireEvent.change(userNameInput, { target: { value: 'ABCDEFGHIJKLMNOPQRSTU' } })
 
+  // get error msg element
   const errorMsg = await screen.findByText(/Your username cannot be longer than 20 characters./i);
 
-
-  // assert: check value; check if error gets shown
+  // assert
+  // check value; check if error gets shown
   expect(userNameInput.value).toBe('ABCDEFGHIJKLMNOPQRSTU');
+  // check if error gets shown
   expect(errorMsg).toBeInTheDocument();
+  // make sure play but is not clickable
+  expect(playButton).toBeDisabled();
 
 });
