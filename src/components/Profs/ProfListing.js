faaabe4466341f4423bd769c14272841d80213cd
T3chSh0ck <35070176+T3chSh0ck@users.noreply.github.com>
2021-03-02 18:06:54 +0100

    ProfListing design changes

--- src/components/Profs/ProfListing.js
+++ src/components/Profs/ProfListing.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../../component-design/Profs/ProfListing.css'
 //<ProfListing prof={{id:0,name:"Herberto", img:"", pop:0,ex:2}}/>
 export default class ProfListing extends React.Component
 {
@@ -17,28 +18,36 @@ export default class ProfListing extends React.Component
    
     
     render() {
+        const testelements = [{image: this.state.img, name: this.state.name, pop: this.state.pop, ex: this.state.ex}, {image: this.state.img, name: this.state.name, pop: this.state.pop, ex: this.state.ex}];
+
+        const reshtml = [];
+
+        for (const [idx, val] of testelements.entries()){
+            reshtml.push(
+            <div className='ProfBumper'>
+                <div class="card" style={{width: "18rem"}}>
+                    <img src={val.image} class="card-img-top" alt="..."/>
+                    <div class="card-body">
+                        <h5 class="card-title">{val.name}</h5>
+                        <p class="card-text">
+                            <ul><li>
+                                Popularity: {val.pop}
+                                </li>
+                                <li>
+                                Exmatriculation:{val.ex}
+                                </li></ul>
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <button type="button" class="btn btn-primary">Add to room</button>
+                    </div>
+                </div>
+            </div>)
+        }
+
         return(
-            <div class="card" style={{width: "18rem"}}>
-                <img src={this.state.img} class="card-img-top" alt="..."/>
-                <div class="card-body">
-                    <h5 class="card-title">{this.state.name}</h5>
-                    <p class="card-text">
-                        <ul><li>
-                            Popularity: {this.state.pop}
-                            </li>
-                            <li>
-                            Exmatriculation:{this.state.ex}
-                            </li></ul>
-                    </p>
-                </div>
-                <div class="card-footer">
-                   {this.props.locked?(
-                       <button type="button" class="btn btn-primary">Unlock</button>
-                   ):(
-                    <button type="button" class="btn btn-primary">Add to room</button>
-                   )} 
-                </div>
-                </div>
+            <div className='ProfList'>{reshtml}</div>
+            
         );
     }
 
