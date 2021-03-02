b6f9c414ce2afcc4b24acbe0ebe3dcbe51f0c02e
xenaletters <62646699+xenaletters@users.noreply.github.com>
2021-03-02 20:13:41 +0100

    Profiles deletable

--- src/components/page/Settings.js
+++ src/components/page/Settings.js
@@ -16,10 +16,28 @@ export default class Settings extends React.Component {
         return (
             <div className="Settings">
                 <div className="spacer"/>
-                Settings
+                <div class="card" style={{margin:"15px"}}>
+                    <div class="card-header" > 
+                        Profile administation
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Delete your profiles</h5>
+                        <p class="card-text">Are you sure? All your data will be irrevocably deleted!</p>
+                        <button type="button" class="btn btn-danger" onClick={()=>{this.deleteAllProfile()}}>Delete all profiles</button>
+                    </div>
+                    </div>
+                
+                
             </div>
         );
     }
+
+    deleteAllProfile(){
+        localStorage.clear();
+        window.location.reload();
+    }
+    
+    
 
 
 }
